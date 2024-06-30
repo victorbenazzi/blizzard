7712b8c307fd8a5a48140f4ad32068448820ef8f
Victor <victormatosbenazzi@gmail.com>
2024-06-30 11:34:28 -0300

    chore: Update page layout with full-screen background

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Header from "./components/Header";
 export default function Home() {
   return (
     <>
-      <Header />
+      <div className="w-full bg-neutral-950 h-screen flex items-start justify-center">
+        <Header />
+      </div>
     </>
   );
 }
