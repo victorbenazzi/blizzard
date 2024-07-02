8e42f9071cdacdbe680d7177055ed1b863fafda7
Victor <victormatosbenazzi@gmail.com>
2024-07-01 22:27:47 -0300

    chore: Update header layout and styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,16 +9,18 @@ import { User } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between w-full max-w-[1220px] h-24">
-      <Image src={LogoBlizzard} alt="Blizzard Logo" width={87} height={40} />
-      <Navbar />
-      <div className="inline-flex gap-x-2">
-        <Button variant="outline"> Criar conta</Button>
-        <Button>
-          {" "}
-          <User size={14} strokeWidth={3} className="mr-2" /> Logar
-        </Button>
-      </div>
-    </header>
+    <div className=" flex items-center justify-center w-full border-b border-neutral-800">
+      <header className="flex items-center justify-between w-full max-w-[1220px] h-24 ">
+        <Image src={LogoBlizzard} alt="Blizzard Logo" width={87} height={40} />
+        <Navbar />
+        <div className="inline-flex gap-x-2">
+          <Button variant="outline"> Criar conta</Button>
+          <Button>
+            {" "}
+            <User size={14} strokeWidth={3} className="mr-2" /> Logar
+          </Button>
+        </div>
+      </header>
+    </div>
   );
 }
